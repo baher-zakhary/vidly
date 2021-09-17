9792c9693ce846fb29009b1cf17a395dcdaff706
BaherWY <baher.wael@gmail.com>
2021-09-17 22:37:14 +0200

    - Refactor read app title from static data file - Rename app to Vidly

--- vidly/Controllers/CustomersController.cs
+++ vidly/Controllers/CustomersController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using vidly.Models;
+using vidly.Static_data;
 
 namespace vidly.Controllers
 {
-    public class HomeController : Controller
+    public class CustomersController : Controller
     {
+        // GET: Customers
         public ActionResult Index()
         {
+            var customers = StaticData.Customers;
             return View();
         }
-
     }
 }
---
+++ vidly/Static_data/StaticData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using vidly.Models;
+
+namespace vidly.Static_data
+{
+    public class StaticData
+    {
+        public readonly static string AppTitle = "Vidly";
+
+        public readonly static List<Customer> Customers = new List<Customer>
+        {
+            new Customer { Name = "John Smith" },
+            new Customer { Name = "Mary Williams" }
+        };
+    }
+}
