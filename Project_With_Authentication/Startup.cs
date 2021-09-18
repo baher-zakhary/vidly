0cff0b720aa34b3d0215f23cbdc5b4d4ae7fda36
BaherWY <baher.wael@gmail.com>
2021-09-18 13:39:28 +0200

    - Add Individual Authentication to project - Add-Migration initialModel

---
+++ Project_With_Authentication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Project_With_Authentication.Startup))]
+namespace Project_With_Authentication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ vidly/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(vidly.Startup))]
+namespace vidly
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
