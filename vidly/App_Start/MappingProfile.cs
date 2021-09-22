f06650ce3920f5e14dd5af5b4c2a791826e5f3de
BaherWY <baher.wael@gmail.com>
2021-09-22 23:01:51 +0200

    - Install package and Setup auto mapper - Setup mapping from CustomerDto to Customer and vice versa

---
+++ vidly/App_Start/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using AutoMapper;
+using vidly.DTOs;
+using vidly.Models;
+
+namespace vidly.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Customer, CustomerDto>();
+            Mapper.CreateMap<CustomerDto, Customer>();
+        }
+    }
+}
