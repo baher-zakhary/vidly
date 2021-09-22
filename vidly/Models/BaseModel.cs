ec4cabaa3ea9ed917dc1ee761ecaa6d7191c9614
BaherWY <baher.wael@gmail.com>
2021-09-22 16:28:03 +0200

    - Implicitly required properties code note

--- vidly/Models/BaseModel.cs
+++ vidly/Models/BaseModel.cs
@@ -9,7 +9,13 @@ namespace vidly.Models
     public class BaseModel
     {
         public int Id { get; set; }
-        [Required]
+
+        /*
+         * strings need required annotation as they can be null,
+         * other types are not nullable by default so they are required implicity
+         * as long as they don't have ? operator ex: int?, byte? ...
+         */
+        [Required]  
         [StringLength(255)]
         public string Name { get; set; }
         public DateTime CreatedOn { get; set; }
