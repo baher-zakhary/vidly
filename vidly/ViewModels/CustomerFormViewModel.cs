41dd0a0ed07c9a1b6ff1d4554af2936b67ec054e
BaherWY <baher.wael@gmail.com>
2021-09-22 10:59:53 +0200

    - Loosely coupled codeing note

--- vidly/ViewModels/CustomerFormViewModel.cs
+++ vidly/ViewModels/CustomerFormViewModel.cs
@@ -8,6 +8,7 @@ namespace vidly.ViewModels
 {
     public class CustomerFormViewModel
     {
+        // We use IEnumerable (interface) to make our code loosely coupled instead of using concrete implementations
         public IEnumerable<MembershipType> MembershipTypes { get; set; }
 
         public Customer Customer { get; set; }
