23a89ec0d4c3478e8d2d0245139ee4950c8d924c
BaherWY <baher.wael@gmail.com>
2021-09-22 22:47:37 +0200

    - Implement Customer Dto

---
+++ vidly/DTOs/CustomerDto.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using vidly.Models;
+using vidly.Models.CustomValidations;
+
+namespace vidly.DTOs
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        /*
+         * There should not be any dependency between our DTOs and our Models (complete decoupling)
+         * If needed create and use MembershipTypeDto instead
+         */
+        //public MembershipType MembershipType { get; set; }
+
+
+        public int MembershipTypeId { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? Birthdate { get; set; }
+    }
+}
