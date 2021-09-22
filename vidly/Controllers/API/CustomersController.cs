0e9ad9609e87373325a393b701642a07e68a5350
BaherWY <baher.wael@gmail.com>
2021-09-22 21:59:49 +0200

    - Customers Web API Initial implementation

---
+++ vidly/Controllers/API/CustomersController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using vidly.Models;
+
+namespace vidly.Controllers.API
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _dbContext { get; set; }
+
+        public CustomersController()
+        {
+            _dbContext = ApplicationDbContext.Create();
+        }
+
+        // GET /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _dbContext.Customers.ToList();
+        }
+
+        // GET /api/customers/{id}
+        public Customer GetCustomer(int id)
+        {
+            var customer = _dbContext.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            return customer;
+        }
+
+        // POST /api/customers
+        [HttpPost] // annotation should be used becase action name is not PostCustomer
+        public Customer CreateCustomer(Customer customer)
+        {
+            if (!ModelState.IsValid == true)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            _dbContext.Customers.Add(customer);
+            _dbContext.SaveChanges();
+            return customer;
+        }
+
+        // PUT /api/customers/1
+        [HttpPut]
+        public Customer UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+            var customerInDb = _dbContext.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            customerInDb.Name = customer.Name;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            customerInDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+            customerInDb.Birthdate = customer.Birthdate;
+            _dbContext.SaveChanges();
+            return customer;
+        }
+
+        // DELETE /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _dbContext.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerInDb == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            _dbContext.Customers.Remove(customerInDb);
+            _dbContext.SaveChanges();
+        }
+    }
+}
--- vidly/Global.asax.cs
+++ vidly/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
+using System.Web.Http;
 using System.Web.Routing;
 
 namespace vidly
@@ -12,6 +13,7 @@ namespace vidly
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
